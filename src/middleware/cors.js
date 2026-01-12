ac57f5098ae17262cd1a23022e8e9872ed268491
Junaid <junaid@crizac.com>
2026-01-12 23:35:54 +0530

    middle ware cros

--- src/middleware/cors.js
+++ src/middleware/cors.js
@@ -1,25 +1,15 @@
 // src/middleware/cors.js
 const cors = require('cors');
 
-const allowedOrigins = (process.env.ALLOWED_ORIGINS || 'http://localhost:4200')
-  .split(',')
-  .map(origin => origin.trim());
-
-const corsOptions = {
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin) return callback(null, true);
-
-    if (allowedOrigins.indexOf(origin) !== -1 || allowedOrigins.includes('*')) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+module.exports = cors({
+  origin: true, // reflects request origin
   credentials: true,
-  optionsSuccessStatus: 200,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization', 'x-hotelid', 'x-app-key']
-};
-
-module.exports = cors(corsOptions);
+  allowedHeaders: [
+    'Content-Type',
+    'Authorization',
+    'x-hotelid',
+    'x-app-key'
+  ],
+  optionsSuccessStatus: 200
+});
